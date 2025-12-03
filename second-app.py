a65e563497d7bd290b565de452bec8c18daa4b7e
ismail41U <ismailsutdonduran@gmail.com>
2025-12-03 20:11:14 +0300

    Second App

---
+++ second-app.py
@@ -0,0 +1,27 @@
+
+Customer_Name = "Customer Name : Ela\n"
+Customer_Surname = "\rCustomer Surname : Sütdonduran"
+Customer_Fullname = "Customer Fullname : Ela Sütdonduran"
+Customer_Fullname_= "Customer Fullname"
+Customer_Fullname_ += "\n\t - Ela Sütdonduran"
+Customer_Gender = "Customer Gender : Female"
+Customer_No = "Customer No : 13579"
+Customer_YearofBirth = 2018
+Customer_Age = 2025 - Customer_YearofBirth
+Customer_Address = "Customer Address : From Turkey-Live in İstanbul"
+
+print(Customer_Name,Customer_Surname)
+print(Customer_Fullname)
+print(Customer_Fullname_)
+print(Customer_Gender)
+print(f"Customer Year of Birth : {Customer_YearofBirth}")
+print(f"Customer Age : {Customer_Age}")
+print(Customer_No)
+print(Customer_Address)
+
+
+order = 110
+order_2 = 110.50
+order_3 = 465.80
+print(f"Total order price :${order + order_2 + float(order_3)}")
+
