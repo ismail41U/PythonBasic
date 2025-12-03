4840337a5052db2cd92bd1122544ab269e23f2d1
ismail41U <ismailsutdonduran@gmail.com>
2025-12-03 20:38:14 +0300

    Add third-app.py file

---
+++ third-app.py
@@ -0,0 +1,7 @@
+pi = 3.14159
+radius = input("the radius: ")
+
+circleOfTheArea = pi * float(radius) ** 2
+circumferenceOfTheCircle = 2 * pi * float(radius)
+print("the area is :", circleOfTheArea)
+print("the circumference is :", circumferenceOfTheCircle)
