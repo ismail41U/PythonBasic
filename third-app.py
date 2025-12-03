de4bf1436f866fe958ee701283c953715720a144
ismail41U <ismailsutdonduran@gmail.com>
2025-12-03 20:39:56 +0300

    Third App.py

--- third-app.py
+++ third-app.py
@@ -5,3 +5,4 @@ circleOfTheArea = pi * float(radius) ** 2
 circumferenceOfTheCircle = 2 * pi * float(radius)
 print("the area is :", circleOfTheArea)
 print("the circumference is :", circumferenceOfTheCircle)
+
