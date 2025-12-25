062fa27f3152df2b9bf29128151187411163d16e
ismail41U <ismailsutdonduran@gmail.com>
2025-12-25 10:52:55 +0300

    Python Basic

---
+++ PYTHON_BASİC/functions.py
@@ -0,0 +1,18 @@
+def changeName(n):
+    n = "Ela"
+
+name = "Kaan"
+
+changeName(name)
+print(name)
+
+print("---------------------------------------------------")
+
+def city(n):
+    n[0] = "Kocaeli"
+
+citys = ["Istanbul","Ankara"]
+
+city(citys)
+print(citys)
+
